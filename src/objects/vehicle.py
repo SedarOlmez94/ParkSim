59815156c78c9f081794cac32ee3b0bbe977928f
SedarOlmez94 <olmez49@gmail.com>
2026-09-13 13:33:17 +0100

    🎨 added position variables and linted.

--- src/objects/vehicle.py
+++ src/objects/vehicle.py
@@ -1,6 +1,6 @@
-'''
+"""
 This module defines the Vehicle class and its associated properties and methods.
-'''
+"""
 
 import mesa
 import numpy as np
@@ -14,6 +14,9 @@ class Vehicle(mesa.Agent):
         self.id = id
         self.fuel_type = fuel_type
         self.max_speed = max_speed
+        # Initialize the position of the vehicle
+        self.posx = 0
+        self.posy = 0
 
     def get_id(self) -> int:
         return self.id
@@ -24,6 +27,9 @@ class Vehicle(mesa.Agent):
     def get_max_speed(self) -> float:
         return self.max_speed
 
+    def get_position(self) -> tuple:
+        return (self.posx, self.posy)
+
     def set_id(self, id: int) -> None:
         self.id = id
 
@@ -33,7 +39,12 @@ class Vehicle(mesa.Agent):
     def set_max_speed(self, max_speed: float) -> None:
         self.max_speed = max_speed
 
+    def update_position(self, posx: float, posy: float) -> None:
+        # Position will be based on the coordinate system of the environment either lat, lng or cartesian coordinates
+        self.posx = posx
+        self.posy = posy
 
-''' TIPS
+
+""" TIPS
 self.model.agents -> a list of agents currently in the model. If you use random.choice(self.model.agents) a random active agent is selected.
-'''
+"""
